eecf409ce90f3964452ba4137594608203d8900f
rashmiw333 <rashmiwankhade99@gmail.com>
2026-06-15 15:12:16 +0530

    delete operations

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ app.use(express.json())
 
 //Task1
 app.get("/",(req,res)=>{
-    res.send("Express server.");
+    res.send("Hello, Express server.");
 })
 
 //Task2
@@ -16,16 +16,19 @@ const movies = [
   { id: 2, title: 'The Godfather', director: 'Francis Ford Coppola', year: 1972 }
 ];
 
-app.post("/movies",(req,res)=>{
-    const newMovie = req.body;
-    if(!newMovie.title || !newMovie.director|| !newMovie.year){
-      res.status(400).json({error: "title,director,year are required"});
-    }else{
-        movies.push(newMovie)
-        res.status(201).json({message: "Movie added Successfully.",movie:newMovie});
+app.delete("/movies/:id",(req,res)=>{
+    const movieId = req.params.id
+    console.log(movieId);
+    const index = movies.findIndex(movie=>movie.id == movieId);
+    console.log(index);
 
+    if(index === -1){
+       return res.status(404).json({error: 'movie Not Found'});
+    } else{
+        movies.splice(index,1)
+        res.status(200).json({message:"Movie deleted Successfully."});
     }
-});
+})
 
 //Task3
 app.get("/movies",(req,res)=>{
@@ -35,19 +38,23 @@ app.get("/movies",(req,res)=>{
 //Task4
 const items = [
  { id: 1, itemName: 'Spoon', color: 'Silver', quantity: 8},
- { id: 2, itemName: 'Fork', color: 'Silver', quantity: 8 }
+ { id: 2, itemName: 'Fork', color: 'Silver', quantity: 8 },
+ { id: 3, itemName: 'Plate', color: 'Off-White', quantity: 6 }
 ];
 
-app.post("/items",(req,res)=>{
-    const newItem = req.body;
+app.delete("/items/:id",(req,res)=>{
+    const itemId = req.params.id
+    console.log(itemId);
+    const index = items.findIndex(item=>item.id == itemId);
+    console.log(index);
 
-    if(!newItem.itemName||!newItem.color||!newItem.quantity){
-        res.status(400).json({error: "itemName,color,quantity are required"});
-    }else{
-       items.push(newItem) 
-       res.status(201).json({message: "Item added Successfully.",item:newItem});
+    if(index === -1){
+       return res.status(404).json({error: 'Item Not Found'});
+    } else{
+        items.splice(index,1)
+        res.status(200).json({message:"Item deleted Successfully."});
     }
-});
+})
 
 //Task5
 app.get("/items",(req,res)=>{
