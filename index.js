c2453462b12c1acad516f192822b34a4ba7ea931
rashmiw333 <rashmiwankhade99@gmail.com>
2026-06-16 14:32:45 +0530

    BE3_Assignment1

--- index.js
+++ index.js
@@ -5,40 +5,69 @@ app.use(express.json())
 
 //Task1
 app.get("/",(req,res)=>{
-    res.send("Express server.");
+    res.send("Hello, This is Express Assignment Server.")
 });
 
 //Task2
-const movies = [
-  { id: 1, title: 'Inception', director: 'Christopher Nolan', year: 2010 },
-  { id: 2, title: 'The Godfather', director: 'Francis Ford Coppola', year: 1972 },
-  { id: 3, title: 'The Shawshank Redemption', director: 'Frank Darabont', year: 1994 }
+const albums = [
+  { id: 1, title: 'Abbey Road', artist: 'The Beatles', year: 1969 },
+  { id: 2, title: 'The Dark Side of the Moon', artist: 'Pink Floyd', year: 1973 },
+  { id: 3, title: 'Thriller', artist: 'Michael Jackson', year: 1982 }
 ];
 
-app.post("/movies/:id",(req,res)=>{
-    const movieId = parseInt(req.params.id)
-    const updatedMovieData = req.body
-
-    const movieToUpdate = movies.find(movie => movie.id === movieId);
-    if(!movieToUpdate){
-        res.status(404).json({error:"Todo not found."})
+app.post("/albums",(req,res)=>{
+    const newAlbum = req.body;
+    if(!newAlbum.title || !newAlbum.artist|| !newAlbum.year){
+      res.status(400).json({error: "title,artist,year are required"});
     }else{
-        if(!updatedMovieData.title || !updatedMovieData.director || !updatedMovieData.year){
-            res.status(400).json({error: "title,director,year are required."})
+        albums.push(newAlbum)
+        res.status(201).json({message: "Album added Successfully.",album:newAlbum});
+
+    }
+});
+
+//Task3
+app.get("/albums",(req,res)=>{
+    res.send(albums);
+});
+
+//Task4
+app.delete("/albums/:id",(req,res)=>{
+    const albumId = req.params.id
+    console.log(albumId);
+    const index = albums.findIndex(item=>item.id == albumId);
+    console.log(index);
+
+    if(index === -1){
+       return res.status(404).json({error: 'Album Not Found'});
+    } else{
+        items.splice(index,1)
+        res.status(200).json({message:"Album deleted Successfully."});
+    }
+})
+
+//Task 5
+app.post("/albums/:id",(req,res)=>{
+    const albumId = parseInt(req.params.id)
+    const updatedAlbumData = req.body
+
+    const albumToUpdate = albums.find(album => album.id === albumId);
+    if(!albumToUpdate){
+        res.status(404).json({error:"Album not found."})
+    }else{
+        if(!updatedAlbumData.title || !updatedAlbumData.artist || !updatedAlbumData.year){
+            res.status(400).json({error: "title,artist,year are required."})
         }else{
-        Object.assign(movieToUpdate,updatedMovieData);
-        res.status(200).json({message:"Movie data updated Successfully.",
-            movie:movieToUpdate})
+        Object.assign(albumToUpdate,updatedAlbumData);
+        res.status(200).json({message:"Album data updated Successfully.",
+            album:albumToUpdate})
         }
         
     }
 });
 
 
-//Task3
-app.get("/movies",(req,res)=>{
-    res.send(movies);
-});
+
 
 //Task4
 const items = [
