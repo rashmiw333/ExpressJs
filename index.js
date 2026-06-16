140abb5676cfe2063a48f7f8e4f7ab8c6241e002
rashmiw333 <rashmiwankhade99@gmail.com>
2026-06-16 22:48:32 +0530

    small update remove extra code

--- index.js
+++ index.js
@@ -67,38 +67,6 @@ app.post("/albums/:id",(req,res)=>{
 });
 
 
-
-
-//Task4
-const items = [
-  { id: 1, itemName: 'Spoon', color: 'Silver', quantity: 8},
-  { id: 2, itemName: 'Fork', color: 'Silver', quantity: 8 },
-  { id: 3, itemName: 'Plate', color: 'Off-White', quantity: 6 }
-];
-
-app.post("/items/:id",(req,res)=>{
-    const itemId = parseInt(req.params.id);
-    const updatedItemData = req.body
-
-    const itemToUpdate = items.find(item => item.id === itemId);
-    if(!itemToUpdate){
-        res.status(404).json({error:"Item not found."})
-    }else{
-        if(!updatedItemData.itemName || !updatedItemData.color || !updatedItemData.quantity){
-            res.status(400).json({error: "itemName,color,quantity are required."})
-        }else{
-        Object.assign(itemToUpdate,updatedItemData);
-        res.status(200).json({message:"Item data updated Successfully.",
-            item:itemToUpdate})
-        }
-        
-    }
-});
-
-app.get("/items",(req,res)=>{
-    res.send(items);
-});
-
 const PORT= 3000
 app.listen(PORT,()=>{
     console.log(`Server running on port ${PORT}`);
